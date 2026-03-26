4ecf1fb0b4af4fc9105b139bb62a4ad5ad2506ec
vasavirao2142 <vasaviraochebrolu2142@gmail.com>
2026-03-26 09:13:54 +0530

    bubble sorting

---
+++ sorting/BubbleSort.js
@@ -0,0 +1,17 @@
+
+function bubbleSort(nums){
+
+    for(let i=0;i<nums.length;i++){
+        for(let j=0;j<nums.length-1;j++){
+            if(nums[j]>nums[j+1]){
+                let temp=nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=temp;
+            }
+        }
+    }
+    return nums;
+}
+
+
+console.log("Array",bubbleSort([5,8,3,2]))
